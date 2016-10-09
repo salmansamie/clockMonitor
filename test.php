116477f5b62cbe8b5bb4ee0a0d0668add428e851
Salman M Rahman <salmansamie@users.noreply.github.com>
2016-10-09 20:24:48 +0100

    fixed: 24hr clock issue

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+
+<head></head>
+
+<body  onload="showLondon()">
+
+<div id="LondonTime"></div>
+<script src="timemonitor.js"></script>
+<php></php>
+
+</body>
+
+</html>
---
+++ timemonitor.js
@@ -0,0 +1,72 @@
+
+function showLondon() {
+
+    var dateHolder = new Date();
+    var h = dateHolder.getHours();
+    var m = dateHolder.getMinutes();
+    var s = dateHolder.getSeconds();
+
+    h =	checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+
+    document.getElementById('LondonTime').style.fontWeight = "900";
+    document.getElementById('LondonTime').style.fontFamily = "open-sans";
+    document.getElementById('LondonTime').style.textAlign = "center"
+    document.getElementById('LondonTime').style.fontSize = "100px"
+    document.getElementById('LondonTime').style.color = "#e58985"
+    document.getElementById('LondonTime').innerHTML = h + ":" + m + ":" + s;
+    
+    var t = setTimeout(showLondon, 0);
+
+}
+
+function showAPJ() {
+
+    var dateHolder = new Date();
+    var h = dateHolder.getHours();
+    var m = dateHolder.getMinutes();
+    var s = dateHolder.getSeconds();
+
+    h = checkTime((h+10)%24);
+    m = checkTime(m);
+    s = checkTime(s);
+
+    document.getElementById('APJTime').style.fontWeight = "900";
+    document.getElementById('APJTime').style.fontFamily = "open-sans";
+    document.getElementById('APJTime').style.textAlign = "center"
+    document.getElementById('APJTime').style.fontSize = "100px"
+    document.getElementById('APJTime').style.color = "#93ffc9"
+    document.getElementById('APJTime').innerHTML = h + ":" + m + ":" + s;
+
+    var t = setTimeout(showAPJ, 0);
+
+}
+
+function showSantaClara() {
+
+    var dateHolder = new Date();
+    var h = dateHolder.getHours();
+    var m = dateHolder.getMinutes();
+    var s = dateHolder.getSeconds();
+
+    h = checkTime((h-8)%24);
+    m = checkTime(m);
+    s = checkTime(s);
+
+    document.getElementById('SantaClaraTime').style.fontWeight = "900";
+    document.getElementById('SantaClaraTime').style.fontFamily = "open-sans";
+    document.getElementById('SantaClaraTime').style.textAlign = "center"
+    document.getElementById('SantaClaraTime').style.fontSize = "100px"
+    document.getElementById('SantaClaraTime').style.color = "#bcf442"
+    document.getElementById('SantaClaraTime').innerHTML = h + ":" + m + ":" + s;
+
+    var t = setTimeout(showSantaClara, 0);
+
+}
+
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
