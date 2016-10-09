e72d9dee20e0dc356aa5651bb615313d1e34b7d8
Salman M Rahman <salmansamie@users.noreply.github.com>
2016-10-09 20:48:53 +0100

    php-activated

--- test.php
+++ test.php
@@ -1,15 +1,37 @@
 <!DOCTYPE html>
-
 <html>
 
-<head></head>
+<head>
+<style>
+p {
+    font-family:"open-sans";
+    font-size:	30px;
+    font-weight: bold;
+    color: #bcf442;
+}
+body {
+    background-image: url();
+    background: url(snow.png) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+</style>
+</head>
 
-<body  onload="showLondon()">
-
-<div id="LondonTime"></div>
+<body  onload="showLondon();showSantaClara();showAPJ();">
 <script src="timemonitor.js"></script>
+
+<p align="center" style="color:#e58985">London</p>
+<div id="LondonTime"></div>
+
+<p align="center" style="color:#93ffc9;">Sydney</p>
+<div id="APJTime"></div>
+
+<p align="center">Santa Clara</p>
+<div id="SantaClaraTime"></div>
+
 <php></php>
-
 </body>
-
 </html>
